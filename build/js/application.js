694a870e6037f1a5bf4ada6b61461f97d4ecf516
Ilkorn <korneev.box@gmail.com>
2015-05-20 18:01:23 +0300

    update header, footer, add module dropdown and navigation

--- build/js/application.js
+++ build/js/application.js
@@ -1,3 +1,30 @@
 /**================================================
-JS : MY CUSTOM SCRIPTS
+JS : INITIALIZATION
 ===================================================*/
+(function($){
+
+    $().ready(function(){
+        // Set navigation behaviour
+        $('.navigation').delegate('.navigation__item', 'click', function(event){
+            var target = $(event.target),
+                navigationItem = target.parents('.navigation__item'),
+                navigationItems = navigationItem.siblings('.navigation__item');
+
+            navigationItems.removeClass('is-active');
+            navigationItem.addClass('is-active');
+        });
+
+        // Set dropdown behaviour
+        $('.dropdown').delegate('.dropdown__caption', 'click', function(event){
+            var target = $(event.target),
+                dropdown = target.parents('.dropdown'),
+                isOpened = dropdown.hasClass('is-open');
+
+            $('.dropdown.is-open').removeClass('is-open');
+            if (!isOpened){
+                dropdown.addClass('is-open');
+            }
+        });
+    });
+
+})($);
